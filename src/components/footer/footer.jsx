a6af48926ff3186ec8d0b569d3c175639389e5cf
IrynaKraikivska <91415017+IrynaKraikivska@users.noreply.github.com>
2022-01-29 09:55:49 +0200

    Add files via upload

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import "./footer.css";
 
-const Footer = () => {
-    return (      
-      <footer class=" footer w-100">
-    
-          {/* <img class=" " src="/images/bottom.png"></img> */}
-          <p class="text-center \">&copy; 2021 Company, Inc</p>
 
-      </footer>
+const Footer = () => {
+    return (
+      <footer className="footer">
+        <img className="footer_wave" src={require("../../pics/wave.png")}/>
+        <p className="footer_text">MusCon - Copyright 2022 © - All rights reserved </p>
+        </footer>
     );
   };
   export default Footer;
-
-
-  
